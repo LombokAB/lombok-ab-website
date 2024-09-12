2c93603d3116ee769bb91d5fbc83a3bef6c2a2c6
KozerT <kozer.vizastaff@gmail.com>
2024-09-12 23:55:33 +0200

    feat: setup react-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { GoCodeOfConduct } from 'react-icons/go';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24  ">
+      <div>
+        <GoCodeOfConduct />
+        <p>Icons test</p>
+      </div>
+    </main>
   );
 }
